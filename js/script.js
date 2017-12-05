138cd23f8cfe5f321d729dccf442ecc92b68ef51
alexlin93 <alexlinica@gmail.com>
2017-12-05 15:31:55 -0500

    updated resume on file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var formattedName = HTMLheaderName.replace("%data%", "Alex Lin");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#home-title").append(formattedName);
+$("#home-title").append(formattedRole);
